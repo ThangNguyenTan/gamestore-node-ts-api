06ecf5b3dfbac7a0170c6438336fb079ae17a9a3
ThangNguyenTan <nguyentanthang1118@gmail.com>
2021-10-27 10:14:28 +0700

    fix(config): modify input for database uri

--- config/database.config.ts
+++ config/database.config.ts
@@ -4,17 +4,28 @@ import config from 'config';
 let db = new Sequelize(config.get('database_test'));
 
 if (config.get('node_env') != 'test') {
-  db = new Sequelize(config.get('database'), config.get('username'), config.get('password'), {
-    host: config.get('host'),
-    dialect: config.get('dialect'),
-    port: 5432,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
+  if (config.get('database_url')) {
+    db = new Sequelize(config.get('database_url'), {
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
       },
-    },
-  });
+    });
+  } else {
+    db = new Sequelize(config.get('database'), config.get('username'), config.get('password'), {
+      host: config.get('host'),
+      dialect: config.get('dialect'),
+      port: 5432,
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+    });
+  }
 }
 
 export default db;
